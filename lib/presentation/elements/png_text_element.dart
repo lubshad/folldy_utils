020227eb27c02ff400e944e786f5d3f27e70b047
lubshad <lubshad4u4@gmail.com>
2022-07-25 17:40:51 +0530

    interactive viewver

--- lib/presentation/elements/png_text_element.dart
+++ lib/presentation/elements/png_text_element.dart
@@ -2,18 +2,17 @@ import 'package:basic_template/basic_template.dart';
 import 'package:flutter/material.dart';
 import 'package:folldy_utils/presentation/elements/element_utils.dart';
 
-import '../../utils/constants.dart';
-
 class PngTextElement extends StatelessWidget {
-  const PngTextElement({Key? key, required this.item})
-      : super(key: key);
+  const PngTextElement({Key? key, required this.item}) : super(key: key);
   final Map<String, dynamic> item;
 
   @override
   Widget build(BuildContext context) {
     return Builder(builder: (context) {
       return item["gallery_image"] == true
-          ? GalleryImageElement(item: item,)
+          ? GalleryImageElement(
+              item: item,
+            )
           : Visibility(
               visible: item["hidden"] != true,
               child: Stack(
@@ -21,7 +20,7 @@ class PngTextElement extends StatelessWidget {
                 children: [
                   if (item["png"] != null)
                     CachedNetworkImage(
-                      imageUrl:  item["png"],
+                      imageUrl: item["png"],
                       fit: BoxFit.values[item["fit"]],
                       cacheManager: Get.find<CacheManager>(),
                     ),
@@ -44,50 +43,60 @@ class PngTextElement extends StatelessWidget {
 class GalleryImageElement extends StatelessWidget {
   final Map<String, dynamic> item;
 
-  const GalleryImageElement(
-      {Key? key, required this.item, required })
+  const GalleryImageElement({Key? key, required this.item, required})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return OpenContainer(
-        closedColor: Colors.transparent,
-        closedElevation: 0,
-        closedBuilder: (context, action) => CachedNetworkImage(
-              imageUrl:  item["png"],
-              fit: BoxFit.values[item["fit"]],
-              cacheManager: Get.find<CacheManager>(),
-            ),
-        openBuilder: ((context, action) => Builder(builder: (context) {
-              Size size = MediaQuery.of(context).size;
+    return InteractiveViewer(
+      child: SizedBox(
+        width: item["width"] * 2,
+        height: item["height"] * 2,
+        child: CachedNetworkImage(
+          imageUrl: item["png"],
+          fit: BoxFit.values[item["fit"]],
+          cacheManager: Get.find<CacheManager>(),
+        ),
+      ),
+    );
+    // return OpenContainer(
+    //     openColor: Colors.transparent,
+    //     closedColor: Colors.transparent,
+    //     closedElevation: 0,
+    //     closedBuilder: (context, action) => CachedNetworkImage(
+    //           imageUrl: item["png"],
+    //           fit: BoxFit.values[item["fit"]],
+    //           cacheManager: Get.find<CacheManager>(),
+    //         ),
+    //     openBuilder: ((context, action) => Builder(builder: (context) {
+    //           Size size = MediaQuery.of(context).size;
 
-              return Container(
-                alignment: Alignment.center,
-                width: size.width,
-                height: size.height,
-                color: Colors.black,
-                child: Stack(
-                  children: [
-                    CachedNetworkImage(
-                      imageUrl:   item["png"],
-                      fit: BoxFit.values[item["fit"]],
-                      cacheManager: Get.find<CacheManager>(),
-                    ),
-                    Positioned(
-                        top: defaultPadding,
-                        right: defaultPadding,
-                        child: IconButton(
-                            onPressed: Get.back, icon: const Icon(Icons.close)))
-                  ],
-                ),
-              );
-            })));
+    //           return Container(
+    //             alignment: Alignment.center,
+    //             width: size.width,
+    //             height: size.height,
+    //             color: Colors.transparent,
+    //             child: Stack(
+    //               children: [
+    //                 CachedNetworkImage(
+    //                   imageUrl: item["png"],
+    //                   fit: BoxFit.values[item["fit"]],
+    //                   cacheManager: Get.find<CacheManager>(),
+    //                 ),
+    //                 Positioned(
+    //                     top: defaultPadding,
+    //                     right: defaultPadding,
+    //                     child: IconButton(
+    //                         onPressed: Get.back, icon: const Icon(Icons.close)))
+    //               ],
+    //             ),
+    //           );
+    //         })));
   }
 }
 
 class PngTextElementThumbnail extends StatelessWidget {
-  const PngTextElementThumbnail(
-      {Key? key, required this.item})
+  const PngTextElementThumbnail({Key? key, required this.item})
       : super(key: key);
   final Map<String, dynamic> item;
 
@@ -99,7 +108,7 @@ class PngTextElementThumbnail extends StatelessWidget {
         children: [
           if (item["png"] != null)
             CachedNetworkImage(
-              imageUrl:item["png"],
+              imageUrl: item["png"],
               fit: BoxFit.values[item["fit"]],
               cacheManager: Get.find<CacheManager>(),
             ),
