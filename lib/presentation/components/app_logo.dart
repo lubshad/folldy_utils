28a15504ff8769818f31fdd99e9a6733b0f11438
lubshad <lubshad4u4@gmail.com>
2022-08-13 11:43:27 +0530

    app logo added

---
+++ lib/presentation/components/app_logo.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class AppLogo extends StatelessWidget {
+  const AppLogo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      "Clez",
+      style: TextStyle(color: Colors.white, fontSize: 80),
+    );
+  }
+}
