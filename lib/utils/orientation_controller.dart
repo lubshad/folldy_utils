7cd03bc605d99d44476c9a389caefc3987c4c3dc
lubshad <lubshad4u4@gmail.com>
2022-11-27 13:48:31 +0530

    Orientation Controller added

---
+++ lib/utils/orientation_controller.dart
@@ -0,0 +1,12 @@
+import 'package:basic_template/basic_template.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class OrientationController extends ChangeNotifier {
+  OrientationController get i => Get.find();
+  DeviceOrientation orientation = DeviceOrientation.portraitUp;
+  changeOrientation(DeviceOrientation newOrientation) {
+    orientation = newOrientation;
+    notifyListeners();
+  }
+}
