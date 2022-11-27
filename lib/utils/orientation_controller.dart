f4ebd1ea3b22e0a27721ffc9924fe752d870106d
lubshad <lubshad4u4@gmail.com>
2022-11-27 13:50:12 +0530

    static

--- lib/utils/orientation_controller.dart
+++ lib/utils/orientation_controller.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class OrientationController extends ChangeNotifier {
-  OrientationController get i => Get.find();
+  static OrientationController get i => Get.find();
   DeviceOrientation orientation = DeviceOrientation.portraitUp;
   changeOrientation(DeviceOrientation newOrientation) {
     orientation = newOrientation;
