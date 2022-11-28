00a347e21b24638bf1aec8a9939bda20449a8d10
lubshad <lubshad4u4@gmail.com>
2022-11-28 20:31:52 +0530

    print

--- lib/presentation/components/recording_screen_app_bar.dart
+++ lib/presentation/components/recording_screen_app_bar.dart
@@ -52,6 +52,7 @@ class RecordingScreenAppBar extends StatelessWidget {
                                       onPressed: minimize,
                                       icon: OrientationBuilder(
                                           builder: (context, orientation) {
+                                        print(orientation);
                                         return Icon(
                                           orientation == Orientation.landscape
                                               ? CupertinoIcons.chevron_back
