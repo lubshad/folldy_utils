ea0e98c61b5518598b1132d2c100c2e71379ffc2
lubshad <lubshad4u4@gmail.com>
2023-10-01 22:31:53 +0530

    parallax effect aaded for background

--- lib/presentation/elements/landscape_page.dart
+++ lib/presentation/elements/landscape_page.dart
@@ -1,6 +1,7 @@
 import 'package:basic_template/basic_template.dart';
 import 'package:flutter/material.dart';
 import 'package:folldy_utils/presentation/elements/element_utils.dart';
+import 'package:folldy_utils/presentation/elements/portrait_page.dart';
 
 class LandscapePage extends StatelessWidget {
   const LandscapePage({
@@ -71,34 +72,37 @@ class LandscapeThumbnail extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        image: item["backgroundImage"] == null
-            ? null
-            : DecorationImage(
-                image: CachedNetworkImageProvider(item["backgroundImage"],
-                    cacheManager: Get.find<CacheManager>()),
-                fit: BoxFit.values[item["fit"]]),
+        // image: item["backgroundImage"] == null
+        //     ? null
+        //     : DecorationImage(
+        //         image: CachedNetworkImageProvider(item["backgroundImage"],
+        //             cacheManager: Get.find<CacheManager>()),
+        //         fit: BoxFit.values[item["fit"]]),
         color: Color(item["color"]),
-        // border: Border.all(color: Colors.black87)
       ),
       height: item["height"],
       width: item["width"],
       child: Builder(builder: (context) {
         List<dynamic> items = item["items"];
         return Stack(
-          // clipBehavior: Clip.none,
-          children: items
-              .map((e) => Positioned(
-                    top: e["top"],
-                    left: e["left"],
-                    height: e["height"],
-                    width: e["width"],
-                    // child: GestureDetector(
-                    //   onTap: e["hidden"] != true && e["topic"] == true
-                    //       ? () => onTap(e)
-                    //       : null,
-                    child: ThumbnailItem(item: e),
-                  ))
-              .toList(),
+          children: [
+            if (item["backgroundImage"] != null)
+              Parallax(
+                  background: CachedNetworkImage(
+                imageUrl: item["backgroundImage"],
+                cacheManager: Get.find<CacheManager>(),
+                fit: BoxFit.values[item["fit"]],
+              )),
+            ...items
+                .map((e) => Positioned(
+                      top: e["top"],
+                      left: e["left"],
+                      height: e["height"],
+                      width: e["width"],
+                      child: ThumbnailItem(item: e),
+                    ))
+                .toList()
+          ],
         );
       }),
     );
--- lib/presentation/elements/portrait_page.dart
+++ lib/presentation/elements/portrait_page.dart
@@ -1,5 +1,6 @@
 import 'package:basic_template/basic_template.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:folldy_utils/presentation/elements/element_utils.dart';
 
 import 'package:palette_generator/palette_generator.dart';
@@ -39,56 +40,34 @@ class PortraitPage extends StatelessWidget {
                     cacheManager: Get.find<CacheManager>()),
                 fit: BoxFit.values[item["fit"]]),
         color: Color(item["color"]),
-        // border: Border.all(color: Colors.black87)
       ),
       height: item["height"],
       width: item["width"],
       child: Builder(builder: (context) {
         List<dynamic> items = item["items"];
-        return Stack(
-            // clipBehavior: Clip.none,
-            children: [
-              FractionallySizedBox(
-                widthFactor: 1,
-                heightFactor: 1,
-                // alignment: Alignment.topLeft,
-                // child: Row(
-                //   children: [
-                //     Expanded(
-                //         child: GestureDetector(
-                //             onTap: previousPage,
-                //             child: Container(
-                //               color: Colors.transparent,
-                //               width: double.infinity,
-                //             ))),
-                //     Expanded(
-                //         child: GestureDetector(
-                //             onTap: nextPage,
-                //             child: Container(
-                //               color: Colors.transparent,
-                //               width: double.infinity,
-                //             ))),
-                //   ],
-                // ),
-                child: GestureDetector(
-                  onTap: onScreenTap,
-                  onLongPress: onLongPress,
-                ),
-              ),
-              ...items
-                  .map((e) => Positioned(
-                      top: e["top"],
-                      left: e["left"],
-                      height: e["height"],
-                      width: e["width"],
-                      child: GestureDetector(
-                        onTap: e["hidden"] != true && e["topic"] == true
-                            ? () => onTap(e)
-                            : null,
-                        child: PresentationItem(item: e),
-                      )))
-                  .toList(),
-            ]);
+        return Stack(children: [
+          FractionallySizedBox(
+            widthFactor: 1,
+            heightFactor: 1,
+            child: GestureDetector(
+              onTap: onScreenTap,
+              onLongPress: onLongPress,
+            ),
+          ),
+          ...items
+              .map((e) => Positioned(
+                  top: e["top"],
+                  left: e["left"],
+                  height: e["height"],
+                  width: e["width"],
+                  child: GestureDetector(
+                    onTap: e["hidden"] != true && e["topic"] == true
+                        ? () => onTap(e)
+                        : null,
+                    child: PresentationItem(item: e),
+                  )))
+              .toList(),
+        ]);
       }),
     );
   }
@@ -105,62 +84,151 @@ class PortraitThumbnail extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        image: item["backgroundImage"] == null
-            ? null
-            : DecorationImage(
-                image: CachedNetworkImageProvider(item["backgroundImage"],
-                    cacheManager: Get.find<CacheManager>()),
-                fit: BoxFit.values[item["fit"]],
-              ),
+        // image: item["backgroundImage"] == null
+        //     ? null
+        //     : DecorationImage(
+        //         image: CachedNetworkImageProvider(item["backgroundImage"],
+        //             cacheManager: Get.find<CacheManager>()),
+        //         fit: BoxFit.values[item["fit"]],
+        //       ),
         color: Color(item["color"]),
-        // border: Border.all(color: Colors.black87)
       ),
       height: item["height"],
       width: item["width"],
       child: Builder(builder: (context) {
         List<dynamic> items = item["items"];
-        return Stack(
-            // clipBehavior: Clip.none,
-            children: [
-              // FractionallySizedBox(
-              //   widthFactor: 1,
-              //   heightFactor: 1,
-              //   alignment: Alignment.topLeft,
-              //   child: Row(
-              //     children: [
-              //       Expanded(
-              //           child: GestureDetector(
-              //               onTap: previousPage,
-              //               child: Container(
-              //                 color: Colors.transparent,
-              //                 width: double.infinity,
-              //               ))),
-              //       Expanded(
-              //           child: GestureDetector(
-              //               onTap: nextPage,
-              //               child: Container(
-              //                 color: Colors.transparent,
-              //                 width: double.infinity,
-              //               ))),
-              //     ],
-              //   ),
-              // ),
-              ...items
-                  .map((e) => Positioned(
-                        top: e["top"],
-                        left: e["left"],
-                        height: e["height"],
-                        width: e["width"],
-                        // child: GestureDetector(
-                        //   onTap: e["hidden"] != true && e["topic"] == true
-                        //       ? () => onTap(e)
-                        //       : null,
-                        child: ThumbnailItem(item: e),
-                      ))
-                  // )
-                  .toList(),
-            ]);
+        return Stack(children: [
+          if (item["backgroundImage"] != null)
+            Parallax(
+                background: CachedNetworkImage(
+              imageUrl: item["backgroundImage"],
+              cacheManager: Get.find<CacheManager>(),
+              fit: BoxFit.values[item["fit"]],
+            )),
+          ...items
+              .map((e) => Positioned(
+                    top: e["top"],
+                    left: e["left"],
+                    height: e["height"],
+                    width: e["width"],
+                    child: ThumbnailItem(item: e),
+                  ))
+              // )
+              .toList(),
+        ]);
       }),
     );
+  }
+}
+
+class Parallax extends SingleChildRenderObjectWidget {
+  const Parallax({
+    super.key,
+    required Widget background,
+  }) : super(child: background);
+
+  @override
+  RenderObject createRenderObject(BuildContext context) {
+    return RenderParallax(scrollable: Scrollable.of(context));
+  }
+
+  @override
+  void updateRenderObject(
+      BuildContext context, covariant RenderParallax renderObject) {
+    renderObject.scrollable = Scrollable.of(context);
+  }
+}
+
+class ParallaxParentData extends ContainerBoxParentData<RenderBox> {}
+
+class RenderParallax extends RenderBox
+    with RenderObjectWithChildMixin<RenderBox>, RenderProxyBoxMixin {
+  RenderParallax({
+    required ScrollableState scrollable,
+  }) : _scrollable = scrollable;
+
+  ScrollableState _scrollable;
+
+  ScrollableState get scrollable => _scrollable;
+
+  set scrollable(ScrollableState value) {
+    if (value != _scrollable) {
+      if (attached) {
+        _scrollable.position.removeListener(markNeedsLayout);
+      }
+      _scrollable = value;
+      if (attached) {
+        _scrollable.position.addListener(markNeedsLayout);
+      }
+    }
+  }
+
+  @override
+  void attach(covariant PipelineOwner owner) {
+    super.attach(owner);
+    _scrollable.position.addListener(markNeedsLayout);
+  }
+
+  @override
+  void detach() {
+    _scrollable.position.removeListener(markNeedsLayout);
+    super.detach();
+  }
+
+  @override
+  void setupParentData(covariant RenderObject child) {
+    if (child.parentData is! ParallaxParentData) {
+      child.parentData = ParallaxParentData();
+    }
+  }
+
+  @override
+  void performLayout() {
+    size = constraints.biggest;
+
+    // Force the background to take up all available width
+    // and then scale its height based on the image's aspect ratio.
+    final background = child!;
+    final backgroundImageConstraints =
+        BoxConstraints.tightFor(width: size.width);
+    background.layout(backgroundImageConstraints, parentUsesSize: true);
+
+    // Set the background's local offset, which is zero.
+    (background.parentData as ParallaxParentData).offset = Offset.zero;
+  }
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    // Get the size of the scrollable area.
+    final viewportDimension = scrollable.position.viewportDimension;
+
+    // Calculate the global position of this list item.
+    final scrollableBox = scrollable.context.findRenderObject() as RenderBox;
+    final backgroundOffset =
+        localToGlobal(size.centerLeft(Offset.zero), ancestor: scrollableBox);
+
+    // Determine the percent position of this list item within the
+    // scrollable area.
+    final scrollFraction =
+        (backgroundOffset.dy / viewportDimension).clamp(0.0, 1.0);
+
+    // Calculate the vertical alignment of the background
+    // based on the scroll percent.
+    final verticalAlignment = Alignment(0.0, scrollFraction * 2 - 1);
+
+    // Convert the background alignment into a pixel offset for
+    // painting purposes.
+    final background = child!;
+    final backgroundSize = background.size;
+    final listItemSize = size;
+    final childRect =
+        verticalAlignment.inscribe(backgroundSize, Offset.zero & listItemSize);
+
+    // Paint the background.
+    context.paintChild(
+        background,
+        (background.parentData as ParallaxParentData).offset +
+            offset +
+            Offset(0.0, childRect.top));
   }
 }
